2ab3c1dc930402419d09755ad6f36b3dd6741ce4
Tamari Jacob Gray-Kaiwahia <tamarigray97@gmail.com>
2019-07-05 14:01:01 +1200

    resolved conflict

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,24 +1,11 @@
 import { combineReducers } from 'redux'
-<<<<<<< HEAD
 import verdict from './verdict'
-=======
-
-import errorMessage from './error-message'
-import subreddits from './subreddits'
-import waiting from './waiting'
->>>>>>> fe8f5793a7d9219da9ec4f06576ac674079f0578
 
 import name from './name'
 import view from './view'
 
 export default combineReducers({
-<<<<<<< HEAD
-  verdict
-=======
-  errorMessage,
-  subreddits,
-  waiting,
+  verdict,
   name,
   view
->>>>>>> fe8f5793a7d9219da9ec4f06576ac674079f0578
 })
