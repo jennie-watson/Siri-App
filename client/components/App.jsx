10a33b1a00c89f1d92412e68f61c5404db1a15de
Raaya Pathare <raaya.pathare7@gmail.com>
2019-07-05 13:24:13 +1200

    accidently included node modules

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
 
-import ErrorMessage from './ErrorMessage'
-import LoadSubreddit from './LoadSubreddit'
-import SubredditList from './SubredditList'
-import WaitIndicator from './WaitIndicator'
-
 import Verdict from './Verdict'
 
 const App = () => (
